8a6c55e30087460cf56dd747f52cb72dba017858
wheydant <vedantkarlekar1@gmail.com>
2023-08-30 12:02:16 +0530

    Hero Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import bannerImg from '../Constants/icons_assets/restauranfood.jpg'
 
 function Hero() {
   return (
-    <div>
-      <h3>Hero</h3>
-    </div>
+    <header>
+      <section>
+        <div className='banner'>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterraneran restaurent, focused on traditional recipies served with a modern twist.</p>
+          <div className='banner-img'>
+            <img src={bannerImg} alt='Banner' />
+          </div>
+          <Link to="/booking"><button aria-label='on Click'>Reserve Table</button></Link>
+        </div>
+      </section>
+    </header>
   );
 }
 
